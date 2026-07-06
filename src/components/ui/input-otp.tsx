5c99704fe57f6e6bcbb4a2ccd7a87e561f17d182
Moses Nyamanyi <mozesthomas75@gmail.com>
2026-07-06 11:32:21 +0300

    updated input-otp

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -1,3 +1,7 @@
+import * as React from "react";
+import { OTPInput, OTPInputContext } from "input-otp";
+import { Minus } from "lucide-react";
+
 import { cn } from "@/lib/utils";
 
 const InputOTP = React.forwardRef<
