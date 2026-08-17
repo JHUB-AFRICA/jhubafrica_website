ab1dea8265e0ac0ed4ca3bc29a04b24812462160
Sonderssss <emmanuelwaweru222199@daystar.ac.ke>
2026-08-17 15:50:57 +0300

    fix: added allowed hosts to the preview

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 export default defineConfig({
   plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
   preview: {
-    allowedHosts: ['.jhubafrica.com', 'localhost'],
+    allowedHosts: ['.jhubafrica.com', 'localhost', 'www.jhubafrica.com', 'jhubafrica.com'],
   },
   server: {
     allowedHosts: ['jhubafrica.com', 'www.jhubafrica.com', 'localhost'],
