b59f10bdc4128413bacbd57bc1e0b7167c11008f
Sonderssss <emmanuelwaweru222199@daystar.ac.ke>
2026-08-17 15:15:18 +0300

    fix: allow localhostin vite preview, fix allowedHosts syntax, wire up tsconfig-paths plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
-  preview: { allowedHosts: ['jhubafrica.com', 'www.jhubafrica.com', 'localhost'] },
-  resolve: {
-    tsconfigPaths: true,
+  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
+  preview: {
+    allowedHosts: ['.jhubafrica.com', 'localhost'],
+  },
+  server: {
+    allowedHosts: ['jhubafrica.com', 'www.jhubafrica.com', 'localhost'],
   },
 })
