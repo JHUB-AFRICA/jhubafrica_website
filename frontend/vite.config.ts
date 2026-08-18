b6d7f4bff21aef2b697956dde2e24d0cbc3d35e2
Sonderssss <emmanuelwaweru222199@daystar.ac.ke>
2026-08-18 06:33:50 +0300

    chore: cleaned up un used imports - resolved the tsconfig path to true removing import

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
+  plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   preview: {
-    allowedHosts: ['.jhubafrica.com', 'localhost', 'www.jhubafrica.com', 'jhubafrica.com'],
+    allowedHosts: ['.jhubafrica.com', 'localhost'],
   },
   server: {
     allowedHosts: ['jhubafrica.com', 'www.jhubafrica.com', 'localhost'],
